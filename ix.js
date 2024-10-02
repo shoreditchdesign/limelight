47339891c273e91ef87a3e462cf0805209550778
shoreditchdesign <webflow@shoreditchdesignstudio.com>
2024-10-03 00:07:36 +0100

    Added countup.js + ScrollTrigger

--- ix.js
+++ ix.js
@@ -64,27 +64,27 @@
 
   ScrollTrigger.refresh();
 
-/*//Countup
+//Countup
 
-$(".counterup").each(function (index) {
-// assign ID
-let thisId = "countup" + index;
-$(this).attr("id", thisId);
-// create variables
-let startNumber = +$(this).text();
-let endNumber = +$(this).attr("final-number");
-let decimals = 0;
-let duration = $(this).attr("count-duration");
-// animate number
-let myCounter = new CountUp(thisId, startNumber, endNumber, decimals, duration);
+  $(".counterup").each(function (index) {
+    // assign ID
+    let thisId = "countup" + index;
+    $(this).attr("id", thisId);
+    // create variables
+    let startNumber = +$(this).text();
+    let endNumber = +$(this).attr("final-number");
+    let decimals = 0;
+    let duration = $(this).attr("count-duration");
+    // animate number
+    let myCounter = new CountUp(thisId, startNumber, endNumber, decimals, duration);
 
-// Scroll into view trigger
-ScrollTrigger.create({
-trigger: $(this),
-start: "top 80%",
-end: "bottom top",
-onEnter: () => {
-myCounter.start();
-}
-});
-});*/
+    // Scroll into view trigger
+    ScrollTrigger.create({
+      trigger: $(this),
+      start: "top 80%",
+      end: "bottom top",
+      onEnter: () => {
+        myCounter.start();
+      }
+    });
+  });
